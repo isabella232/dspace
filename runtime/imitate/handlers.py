7d004221f473d68f2115107ac56084c8edf190dc
silveryfu <silveryfu@gmail.com>
2020-11-29 23:00:26 -0800

    Fix cluster role

--- runtime/imitate/handlers.py
+++ runtime/imitate/handlers.py
@@ -1,7 +1,13 @@
 import kopf
 
 
-@kopf.on.create('zalando.org', 'v1', 'kopfexamples')
+@kopf.on.create('digi.dev', 'v1', 'mockrooms')
 def create_fn(spec, **kwargs):
     print(f"And here we are! Creating: {spec}")
+    return {'message': 'hello world'}  # will be the new status
+
+
+@kopf.on.update('digi.dev', 'v1', 'mocklamps')
+def update_fn(spec, **kwargs):
+    print(f"And here we are! Updating: {spec}")
     return {'message': 'hello world'}  # will be the new status
