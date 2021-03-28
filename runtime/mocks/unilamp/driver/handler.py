9d50c185eae8725ba540bcd769554cad05555c2d
silveryfu <silveryfu@gmail.com>
2021-03-28 15:05:46 -0700

    [mock] refactor lamp

--- runtime/mocks/unilamp/driver/handler.py
+++ runtime/mocks/unilamp/driver/handler.py
@@ -6,6 +6,38 @@ from util import put, first_attr, first_type
 """Universal lamp translates power and brightness 
 to vendor specific lamps."""
 
+# converters
+converters = {
+    "mock.digi.dev/v1/lamps": {
+        "power": {
+            "from": lambda x: x,
+            "to": lambda x: x,
+        },
+        "brightness": {
+            "from": lambda x: x,
+            "to": lambda x: x,
+        }
+    },
+    "mock.digi.dev/v1/colorlamps": {
+        "power": {
+            "from": lambda x: "on" if x == 1 else "off",
+            "to": lambda x: 1 if x == "on" else 0,
+        },
+        "brightness": {
+            "from": lambda x: x / 255,
+            "to": lambda x: x * 255,
+        },
+        "hue": {
+            "from": lambda: -1,
+            "to": lambda: -1,
+        },
+        "saturation": {
+            "from": lambda: -1,
+            "to": lambda: -1,
+        }
+    },
+}
+
 
 # validation
 @on.mount
@@ -18,25 +50,46 @@ def h(lamp_types):
         f"{count}"
 
 
+# intent back-prop
+@on.mount
+def h(parent, bp):
+    ul = parent
+
+    for _, child_path, old, new in bp:
+        typ, attr = child_path[2], child_path[-2]
+
+        assert typ in converters, typ
+
+        put(path=f"control.{attr}.intent",
+            src=new, target=ul,
+            transform=converters[typ][attr]["from"])
+
+
 # status
 @on.mount("lamps")
-def h(lp, ul):
+def h(lp, ul, typ):
     lp = first_attr("spec", lp)
 
-    put("control.power.status", lp, ul)
+    assert typ in converters, typ
 
-    put("control.brightness.status", lp, ul)
+    put(f"control.power.status", lp, ul,
+        transform=converters[typ]["power"]["from"])
+
+    put(f"control.brightness.status", lp, ul,
+        transform=converters[typ]["brightness"]["from"])
 
 
 @on.mount("colorlamps")
-def h(lp, ul):
+def h(lp, ul, typ):
     lp = first_attr("spec", lp)
 
-    put("control.power.status", lp, ul,
-        transform=lambda x: "on" if x == 1 else "off")
+    assert typ in converters, typ
 
-    put("control.brightness.status", lp, ul,
-        transform=lambda x: x / 255)
+    put(f"control.power.status", lp, ul,
+        transform=converters[typ]["power"]["from"])
+
+    put(f"control.brightness.status", lp, ul,
+        transform=converters[typ]["brightness"]["from"])
 
 
 # intent
@@ -44,28 +97,14 @@ def h(lp, ul):
 @on.control
 def h(parent, child):
     ul, lp = parent, first_attr("spec", child)
-    lt = first_type(child)
+    if lp is None:
+        return
 
-    if lt == _gvr("lamps"):
-        put("control.power.intent", ul, lp)
+    typ = first_type(child)
+    assert typ in converters, typ
 
-        put("control.brightness.intent", ul, lp)
+    put(f"control.power.intent", ul, lp,
+        transform=converters[typ]["power"]["to"])
 
-    elif lt == _gvr("colorlamps"):
-        put("control.power.intent", ul, lp,
-            transform=lambda x: 1 if x == "on" else 0)
-
-        put("control.brightness.intent", ul, lp,
-            transform=lambda x: x * 255)
-
-    # more lamps
-    ...
-
-
-def _gvr(s):
-    if len(s.split("/")) == 1:
-        return "/".join([os.environ["GROUP"],
-                         os.environ["VERSION"],
-                         s])
-    else:
-        return s
+    put(f"control.brightness.intent", ul, lp,
+        transform=converters[typ]["brightness"]["to"])
