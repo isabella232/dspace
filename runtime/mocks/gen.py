a8081b3a7e14e4602330a2f204528fdb071a24be
silveryfu <silveryfu@gmail.com>
2021-03-28 20:16:09 -0700

    nits

--- runtime/mocks/gen.py
+++ runtime/mocks/gen.py
@@ -153,16 +153,26 @@ image: silveryfu/{image}:latest
 _handler = """import on
 
 # validation
-...
+@on.attr
+def h():
+    ...
 
 # intent back-prop
-...
+@on.mount
+def h():
+    ...
 
 # status
-...
+@on.mount
+def h():
+    ...
 
 # intent
-...
+@on.mount
+@on.control
+def h():
+    ...
+
 """
 
 
