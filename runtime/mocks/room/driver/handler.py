2f40533eeeb3c2150d39963672898e17fbd3eb2d
silveryfu <silveryfu@gmail.com>
2021-03-30 00:57:38 -0700

    [mock] add room status handling

--- runtime/mocks/room/driver/handler.py
+++ runtime/mocks/room/driver/handler.py
@@ -1,6 +1,6 @@
 import on
 
-from util import put, first_type, first_attr, full_gvr
+from util import put, deep_get, deep_set
 
 """
 Room digivice:
@@ -28,6 +28,8 @@ mode_config = {
     "sleep": {
         "lamps": {
             "power": "off",
+            "brightness": 0.0,
+            "ambiance_color": "blue",
         }
     },
     "idle": {
@@ -36,6 +38,29 @@ mode_config = {
             "brightness": 0.2,
             "ambiance_color": "yellow",
         }
+    }
+}
+
+lamp_converters = {
+    ul_gvr: {
+        "power": {
+            "from": lambda x: x,
+            "to": lambda x: x,
+        },
+        "brightness": {
+            "from": lambda x: x,
+            "to": lambda x: x,
+        }
+    },
+    cl_gvr: {
+        "power": {
+            "from": lambda x: "on" if x == 1 else "off",
+            "to": lambda x: 1 if x == "on" else 0,
+        },
+        "brightness": {
+            "from": lambda x: x / 255,
+            "to": lambda x: x * 255,
+        },
     }
 }
 
@@ -49,10 +74,38 @@ def h():
     pass
 
 
-# # status
+# status
 @on.mount
 def h(parent, mounts):
-    pass
+    room, devices = parent, mounts
+    mode = deep_get(room, "control.mode.intent")
+    bright = deep_get(room, "control.brightness.intent")
+
+    # Handle mode
+    if mode is not None:
+
+        # check lamps
+        for lt in [ul_gvr, cl_gvr]:
+
+            # iterate over individual lamp
+            for n, _l in devices.get(lt, {}).items():
+                if "spec" not in _l:
+                    continue
+                _l = _l["spec"]
+
+                matched = True
+                for attr in ["power", "brightness"]:
+                    convert = lamp_converters[lt][attr]["from"]
+                    if mode_config[mode]["lamps"][attr] != \
+                            convert(deep_get(_l, f"control.{attr}.intent")):
+                        matched = False
+                deep_set(room, f"control.mode.status", mode if matched else "undef")
+
+    # - other devices
+    ...
+
+    # Handle brightness
+    # - check lamps
 
 
 # intent
@@ -60,24 +113,28 @@ def h(parent, mounts):
 @on.control("mode")
 def h(parent, mounts):
     room, devices = parent, mounts
-    mode = room["control"]["mode"]["intent"]
+    mode = deep_get(room, "control.mode.intent")
+
+    if mode is None:
+        return
+
+    # TBD use agg brightness for mode
 
     # handle lamps
-    for lt, power_cnv in [(ul_gvr, lambda x: x),
-                          (cl_gvr, lambda x: 1 if x == "on" else 0)]:
+    for lt in [ul_gvr, cl_gvr]:
 
         # iterate over individual lamp
         for n, _l in devices.get(lt, {}).items():
             if "spec" not in _l:
                 continue
-
             _l = _l["spec"]
 
-            # set lamp power
-            put("control.power.intent",
-                mode_config[mode]["lamps"]["power"], _l,
-                transform=power_cnv)
-    
+            # configure lamp set-points
+            for attr in ["power", "brightness"]:
+                put(f"control.{attr}.intent",
+                    mode_config[mode]["lamps"][attr], _l,
+                    transform=lamp_converters[lt][attr]["to"])
+
     # other devices
     ...
 
@@ -86,20 +143,19 @@ def h(parent, mounts):
 @on.control("brightness")
 def h(parent, mounts):
     room, devices = parent, mounts
-    bright = room["control"]["brightness"]["intent"]
-
-    # handle lamps
+    bright = deep_get(room, "control.brightness.intent")
     lamp_count = len(devices.get(ul_gvr, {})) + \
                  len(devices.get(cl_gvr, {}))
 
-    if lamp_count == 0:
+    if bright is None or lamp_count == 0:
         return
+
+    # handle lamps
 
     div = bright / lamp_count
 
     # set div for each lamp
-    for lt, bright_cnv in [(ul_gvr, lambda x: x),
-                           (cl_gvr, lambda x: x * 255)]:
+    for lt in [ul_gvr, cl_gvr]:
 
         for n, _l in devices.get(lt, {}).items():
             if "spec" not in _l:
@@ -108,7 +164,7 @@ def h(parent, mounts):
             _l = _l["spec"]
 
             put("control.brightness.intent", div, _l,
-                transform=bright_cnv)
+                transform=lamp_converters[lt]["brightness"]["to"])
 
     # other devices
     ...
