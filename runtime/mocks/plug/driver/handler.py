ba221688ac19940e7147cbb65fdde1e4aba45996
silveryfu <silveryfu@gmail.com>
2021-04-08 18:53:40 -0700

    [mock] improve plug

--- runtime/mocks/plug/driver/handler.py
+++ runtime/mocks/plug/driver/handler.py
@@ -1,10 +1,11 @@
 import on
 
-# validation
-...
+from util import deep_set, deep_get
+
 
 # status
-...
-
-# intent
-...
+@on.control
+def h(sv, pv):
+    for _attr, _ in sv.items():
+        deep_set(pv, f"control.{_attr}.status",
+                 deep_get(pv, f"control.{_attr}.intent"))
