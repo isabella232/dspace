abfe9f7da3d70e4cdf1418301abe23081d92552a
silveryfu <silveryfu@gmail.com>
2021-04-12 16:24:55 -0700

    [runtime] remove shadow

--- runtime/shadow/opa.go
+++
@@ -1,5 +0,0 @@
-package shadow
-
-// TODO: check whether/how the kube-mgmt of the OPA project to be used for writing access policies
-//  in dSpace (perhaps chagne it as dSpace-mgmt)
-
--- runtime/shadow/shadow.go
+++
@@ -1,3 +0,0 @@
-package shadow
-
-/* Admission controller webhooks for shadow ACL checks */
